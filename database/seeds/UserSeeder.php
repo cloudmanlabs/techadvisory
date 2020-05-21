794d552fdf08d5efb292555dcf5451d44a8ef7d2
guimcaballero <guim.caballero@gmail.com>
2020-05-21 10:54:14 +0200

    Disabled seeders

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,11 +14,6 @@ class UserSeeder extends Seeder
     public function run()
     {
         factory(User::class)
-            ->states(['admin','guimEmail'])
-            ->create([
-                'name' => 'Guim',
-            ]);
-        factory(User::class)
             ->states('admin')
             ->create([
                 'name' => 'Admin',
@@ -46,37 +41,37 @@ class UserSeeder extends Seeder
             ]);
 
         // Create some other randoms
-        factory(User::class, 3)
-            ->states(['client', 'finishedSetup'])
-            ->create();
-        factory(User::class, 4)
-            ->states(['vendor', 'finishedSetup'])
-            ->create()
-            ->each(function($vendor){
-                $vendor->vendorSolutions()->save(factory(VendorSolution::class)->create([
-                    'vendor_id' => $vendor->id
-                ]));
-            });
+        // factory(User::class, 3)
+        //     ->states(['client', 'finishedSetup'])
+        //     ->create();
+        // factory(User::class, 4)
+        //     ->states(['vendor', 'finishedSetup'])
+        //     ->create()
+        //     ->each(function($vendor){
+        //         // $vendor->vendorSolutions()->save(factory(VendorSolution::class)->create([
+        //         //     'vendor_id' => $vendor->id
+        //         // ]));
+        //     });
 
 
         // Create some that haven't finished set up
-        factory(User::class)
-            ->states('client')
-            ->create([
-                'name' => 'New',
-                'email' => 'new@client.com',
-            ]);
-        factory(User::class)
-            ->states('vendor')
-            ->create([
-                'name' => 'New',
-                'email' => 'new@vendor.com',
-            ])
-            ->each(function ($vendor) {
-                $vendor->vendorSolutions()->save(factory(VendorSolution::class)->create([
-                    'vendor_id' => $vendor->id
-                ]));
-            });
+        // factory(User::class)
+        //     ->states('client')
+        //     ->create([
+        //         'name' => 'New',
+        //         'email' => 'new@client.com',
+        //     ]);
+        // factory(User::class)
+        //     ->states('vendor')
+        //     ->create([
+        //         'name' => 'New',
+        //         'email' => 'new@vendor.com',
+        //     ])
+        //     ->each(function ($vendor) {
+        //         // $vendor->vendorSolutions()->save(factory(VendorSolution::class)->create([
+        //         //     'vendor_id' => $vendor->id
+        //         // ]));
+        //     });
 
 
 
