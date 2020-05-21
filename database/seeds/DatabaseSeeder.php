5e2f71f545e16e019fae62dec71eb6b99d0503da
guimcaballero <guim.caballero@gmail.com>
2020-05-21 10:57:19 +0200

    Reenabled some seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,21 +15,21 @@ class DatabaseSeeder extends Seeder
         // Remove all folders
         Storage::disk('public')->deleteDirectory('folders');
 
-        // $this->call(ClientProfileQuestionSeeder::class);
-        // $this->call(VendorProfileQuestionSeeder::class);
+        $this->call(ClientProfileQuestionSeeder::class);
+        $this->call(VendorProfileQuestionSeeder::class);
         $this->call(UserSeeder::class);
 
         $this->call(PracticeSeeder::class);
         // $this->call(SubpracticeSeeder::class);
 
-        // $this->call(GeneralInfoQuestionSeeder::class);
-        // $this->call(SizingQuestionSeeder::class);
-        // $this->call(SelectionCriteriaQuestionSeeder::class);
+        $this->call(GeneralInfoQuestionSeeder::class);
+        $this->call(SizingQuestionSeeder::class);
+        $this->call(SelectionCriteriaQuestionSeeder::class);
 
         // $this->call(ProjectSeeder::class);
         // $this->call(VendorApplicationSeeder::class);
 
-        // $this->call(VendorSolutionQuestionSeeder::class);
+        $this->call(VendorSolutionQuestionSeeder::class);
 
         // $this->call(SettingsSeeder::class);
     }
