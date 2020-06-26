276f0abbaccaf43222ae56071b54a8e660eafa58
guimcaballero <guim.caballero@gmail.com>
2020-06-26 17:00:11 +0200

    Added titles to nonbinding implementation

--- resources/views/components/selectionCriteria/nonBindingImplementation.blade.php
+++ resources/views/components/selectionCriteria/nonBindingImplementation.blade.php
@@ -6,6 +6,14 @@ $disabled = $disabled ?? false;
 
 <div>
     <label for="projectName">Overall Implementation cost</label>
+    <div style="display: flex; flex-direction: row; justify-content: space-evenly; width: 100%">
+        <div style="width: 50%; padding: 0 0.5rem">
+            <p>Min</p>
+        </div>
+        <div style="width: 50%; padding: 0 0.5rem">
+            <p>Max</p>
+        </div>
+    </div>
     <div style="display: flex; flex-direction: row">
         <input type="number" class="form-control nonBindingInput" placeholder="Min" data-changing="overallImplementationMin"
             value="{{$vendorApplication->overallImplementationMin}}" required
@@ -19,6 +27,14 @@ $disabled = $disabled ?? false;
 <br>
 <div>
     <label for="projectName">Total Staffing cost (%)</label>
+    <div style="display: flex; flex-direction: row; justify-content: space-evenly; width: 100%">
+        <div style="width: 50%; padding: 0 0.5rem">
+            <p>Percentage</p>
+        </div>
+        <div style="width: 50%; padding: 0 0.5rem">
+            <p>Comments</p>
+        </div>
+    </div>
     <div style="display: flex; flex-direction: row">
         <input type="number" class="form-control nonBindingInput" placeholder="Percentage" data-changing="staffingCostNonBinding"
             value="{{$vendorApplication->staffingCostNonBinding}}" required
@@ -32,6 +48,14 @@ $disabled = $disabled ?? false;
 <br>
 <div>
     <label for="projectName">Total Travel cost (%)</label>
+    <div style="display: flex; flex-direction: row; justify-content: space-evenly; width: 100%">
+        <div style="width: 50%; padding: 0 0.5rem">
+            <p>Percentage</p>
+        </div>
+        <div style="width: 50%; padding: 0 0.5rem">
+            <p>Comments</p>
+        </div>
+    </div>
     <div style="display: flex; flex-direction: row">
         <input type="number" class="form-control nonBindingInput" placeholder="Percentage" data-changing="travelCostNonBinding"
             value="{{$vendorApplication->travelCostNonBinding}}" required
@@ -45,6 +69,14 @@ $disabled = $disabled ?? false;
 <br>
 <div>
     <label for="projectName">Total Additional Cost (%)</label>
+    <div style="display: flex; flex-direction: row; justify-content: space-evenly; width: 100%">
+        <div style="width: 50%; padding: 0 0.5rem">
+            <p>Percentage</p>
+        </div>
+        <div style="width: 50%; padding: 0 0.5rem">
+            <p>Comments</p>
+        </div>
+    </div>
     <div style="display: flex; flex-direction: row">
         <input type="number" class="form-control nonBindingInput" placeholder="Percentage" data-changing="additionalCostNonBinding"
             value="{{$vendorApplication->additionalCostNonBinding}}" required
