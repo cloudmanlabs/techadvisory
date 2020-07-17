0d9b80203a6df0e6aa46545cb2ac473e288876b7
guimcaballero <guim.caballero@gmail.com>
2020-07-17 12:57:18 +0200

    Implemented extracting analytics by section

--- app/Exports/Sheets/AnalyticsExportSheet.php
+++ app/Exports/Sheets/AnalyticsExportSheet.php
@@ -4,7 +4,9 @@ namespace App\Exports\Sheets;
 
 use App\SelectionCriteriaQuestionResponse;
 use App\Project;
+use App\SelectionCriteriaQuestion;
 use App\VendorApplication;
+use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithTitle;
 
@@ -29,52 +31,62 @@ class AnalyticsExportSheet implements FromCollection, WithTitle
     {
         // What a fucking mess this is. Sorry :)
 
-        $vendors = $this->project->vendorApplications;
+        $applications = $this->project->vendorApplications->sortBy(function ($application, $key) {
+            return $application->vendor->id;
+        });
 
-        $firstRow = $vendors
-            ->sortBy(function ($application, $key) {
-                return $application->vendor->id;
+        $firstRow = [
+            'Question' => 'Question'
+        ];
+        foreach ($applications as $key => $application) {
+            $firstRow[] = $application->vendor->name . ' Response';
+            $firstRow[] = $application->vendor->name . ' Score';
+        }
+
+        $rows = [];
+
+        $questionsByPage = $this->project->selectionCriteriaQuestions
+            ->filter(function (SelectionCriteriaQuestionResponse $response) {
+                return in_array($response->originalQuestion->page, $this->pages);
             })
-            ->map(function (VendorApplication $application) {
-                return [
-                    'Vendor Response' => $application->vendor->name . ' Response',
-                    'Vendor Score' => $application->vendor->name . ' Score',
+            ->groupBy(function (SelectionCriteriaQuestionResponse $response, $key) {
+                return $response->originalQuestion->page;
+            });
+
+        foreach ($questionsByPage as $page => $questions) {
+            $rows[] = [
+                SelectionCriteriaQuestion::pagesSelect[$page] ?? ''
+            ];
+            $rows[] = $firstRow;
+
+            $questionsGrouped = $questions->groupBy(function ($question) {
+                return $question->originalQuestion->id;
+            });
+
+            foreach ($questionsGrouped as $key => $responses) {
+                $return = [
+                    $responses[0]->originalQuestion->label,
                 ];
-            })
-            ->prepend([
-                'Question' => 'Question',
-            ])
-            ->flatten();
 
-        $rows = $this->project->selectionCriteriaQuestions
-                ->filter(function (SelectionCriteriaQuestionResponse $response) {
-                    return in_array($response->originalQuestion->page, $this->pages);
-                })
-                ->groupBy(function (SelectionCriteriaQuestionResponse $response, $key) {
-                    return $response->originalQuestion->id;
-                })
-                ->filter(function($arrayOfResponses){
-                    return count($arrayOfResponses) > 0;
-                })
-                ->map(function($arrayOfResponses){
-                    return collect($arrayOfResponses)
-                            ->sortBy(function (SelectionCriteriaQuestionResponse $response, $key) {
-                                return $response->vendor->id;
-                            })
-                            ->map(function(SelectionCriteriaQuestionResponse $response){
-                                return [
-                                    'Vendor Response' => $response->response,
-                                    'Vendor Score' => $response->score,
-                                ];
-                            })
-                            ->flatten()
-                            ->prepend($arrayOfResponses[0]->originalQuestion->label)
-                            ->toArray();
-                });
+                foreach ($applications as $key => $application) {
+                    $response = $responses->filter(function($response) use ($application){
+                        return $response->vendor->is($application->vendor);
+                    })->first();
+                    if($response == null){
+                        $return[] = '';
+                        $return[] = '';
+                    }
+                    $return[] = $response->response ?? '';
+                    $return[] = $response->score ?? '';
+                }
 
-        $rows->prepend($firstRow);
+                $rows[] = $return;
+            }
 
-        return $rows;
+            $rows[] = [''];
+        }
+
+        return collect($rows);
     }
 
     /**
