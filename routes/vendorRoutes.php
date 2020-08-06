bfef932cebcb410c28203b3475f8146a91e7ac4a
Miguel <miguelg@microbio.tv>
2020-08-06 12:25:47 +0200

    Consulta Projects -1 year

--- routes/vendorRoutes.php
+++ routes/vendorRoutes.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use App\Project;
+use Carbon\Carbon;
 
 
 
@@ -39,7 +41,7 @@ Route::prefix('vendors')
                     ->name('profile.changeResponse');
             });
 
-            Route::middleware(['checkUserHasFinishedSetup'])->group(function(){
+            Route::middleware(['checkUserHasFinishedSetup'])->group(function () {
                 Route::get('home', 'HomeController@home')
                     ->name('home');
                 Route::get('profile', 'ProfileController@profile')
@@ -59,7 +61,6 @@ Route::prefix('vendors')
 
                 Route::get('submittedApplication/{project}', 'ProjectController@submittedApplication')
                     ->name('submittedApplication');
-
 
 
                 Route::post('application/setRejected/{project}', 'ProjectController@setRejected')
@@ -84,6 +85,58 @@ Route::prefix('vendors')
                     ->name('profile.changeSolutionName');
                 Route::post('solution/changePractice', 'SolutionController@changeSolutionPractice')
                     ->name('profile.changeSolutionPractice');
+
+
+                //      ***   TESTING SPRINT 1 PUNTO 1 **********************************************************
+                Route::get('example', function () {
+                    return $result = "Hola";
+                });
+
+
+                // Test if DB retrieve data of Table project. (using Eloquent)
+                // Note: min id = 7
+                Route::get('firstProject', function () {
+                    return App\Project::find(7)->name;
+                });
+
+
+                // Test if DB retrieve data with parameters of Table project and pretty print well
+                // Note: min id = 7
+                Route::get('project/{id}', function ($id) {
+                    $project = Project::find($id);
+                    //echo '<pre>' . var_export($project, true) . '</pre>';     // print
+
+
+                    //echo $project;
+                });
+
+                // test advanced querys for table project.
+                Route::get('project365',function (){
+
+                    $current_date = date('Y-m-d H:i:s');
+
+                    // Date range limit
+                    $date_limit = Carbon::parse($current_date)->addMonths(-12)->format('Y-m-d');
+
+                    // Query: Last year proyects filtered
+                    $projects = App\Project::select('id','name','created_at')
+                        ->where('industry','Automative')
+                        ->whereDate('created_at', '<',$date_limit)
+                        ->orderby('created_at')
+                        ->first();
+
+                    var_dump($projects);
+                    die();
+
+
+                });
+
+                // test one to one relations.
+                Route::get('/project/{id}/client', function ($id) {
+                    return Project::find($id)->client_id;
+                });
             });
         });
     });
+
+
