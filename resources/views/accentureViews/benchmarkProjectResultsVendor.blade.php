46eda3c252c7c3de9b19dc70590be9acaea135dc
Miguel <miguelg@microbio.tv>
2020-10-29 08:46:44 +0100

    fix: Project results - vendor: Only can filter by One Practice

--- resources/views/accentureViews/benchmarkProjectResultsVendor.blade.php
+++ resources/views/accentureViews/benchmarkProjectResultsVendor.blade.php
@@ -165,7 +165,9 @@
     @parent
     <script>
 
-        $('#practices-select').select2();
+        $('#practices-select').select2({
+            maximumSelectionLength: 1
+        });
         $('#subpractices-select').select2();
         $('#years-select').select2();
         $('#industries-select').select2();
