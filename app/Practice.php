8ee76e2d5e085e14ecc9a0781a47e1cb329f5e95
Miguel <miguelg@microbio.tv>
2020-11-11 10:36:26 +0100

    feat: Benchmark - Overview - General: Added new chart: Shows Number of vendors released from old projects by each practice / total old projects by each practice

--- app/Practice.php
+++ app/Practice.php
@@ -35,7 +35,7 @@ class Practice extends Model
     public function applicationsInProjectsWithThisPractice($regions = [], $years = [])
     {
 
-        $query = $this->projects()->select('id', 'currentPhase','regions', 'created_at');
+        $query = $this->projects()->select('id', 'currentPhase', 'regions', 'created_at');
         $query = $this->benchmarkOverviewFilters($query, $regions, $years);
 
         $query = $query->get()->map(function (Project $project) {
@@ -46,9 +46,38 @@ class Practice extends Model
 
     }
 
+    public function averageReleasedApplicationsInOldProjectsWithThisPractice($regions = [], $years = [])
+    {
+
+        $query = $this->projects()->select('id', 'currentPhase', 'regions', 'created_at');
+        $query = $this->benchmarkOverviewFilters($query, $regions, $years);
+
+        // Total projects
+        $totalOldProjects = $query
+            ->where('currentPhase', '=', 'old')
+            ->count();
+
+        // Number of vendors
+        $applicationsReleasedForOldProjectsWithThisPractice = $query->get()
+            ->map(function (Project $project) {
+                return $project->vendorApplications
+                    ->where('phase', '=', 'evaluated')
+                    ->count();
+            })->sum();
+
+        $average = 0;
+        if ($totalOldProjects > 0) {
+            // Vendors / Total projects.
+            $average = $applicationsReleasedForOldProjectsWithThisPractice / $totalOldProjects;
+        }
+
+        return $average;
+    }
+
+
     public function numberOfProjectsByVendor(User $vendor, $regions = [], $years = [])
     {
-        $query = $this->projects()->select('id', 'currentPhase','regions', 'created_at');
+        $query = $this->projects()->select('id', 'currentPhase', 'regions', 'created_at');
         $query = $this->benchmarkOverviewFilters($query, $regions, $years);
 
         $query = $query->get()->filter(function (Project $project) use ($vendor) {
@@ -61,7 +90,7 @@ class Practice extends Model
     // Encapsulate the filters for graphics from view: Overview - general
     private function benchmarkOverviewFilters($query, $regions = [], $years = [])
     {
-        $query = $query->where('currentPhase','=','old');
+        $query = $query->where('currentPhase', '=', 'old');
 
         if ($regions) {
             $query = $query->where(function ($query) use ($regions) {
--- resources/views/accentureViews/benchmarkOverview.blade.php
+++ resources/views/accentureViews/benchmarkOverview.blade.php
@@ -178,7 +178,7 @@
                         backgroundColor: ["#27003d", "#5a008f", "#8e00e0", "#a50aff", "#d285ff", "#e9c2ff", "#f8ebff"],
                         data: [
                             @foreach($practices as $practice)
-                                "{{$practice->applicationsInProjectsWithThisPractice($regionsToFilter,$yearsToFilter)}}",
+                                "{{$practice->averageReleasedApplicationsInOldProjectsWithThisPractice($regionsToFilter,$yearsToFilter)}}",
                             @endforeach
                         ]
                     }
