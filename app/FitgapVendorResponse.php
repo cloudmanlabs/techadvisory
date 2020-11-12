77a486219784290bbe453d8e3d7e65345cfae986
Miguel <miguelg@microbio.tv>
2020-11-12 13:49:55 +0100

    fix: FitgapVendorResponse.php Model completed

--- app/FitgapVendorResponse.php
+++ app/FitgapVendorResponse.php
@@ -10,7 +10,11 @@ use phpseclib\System\SSH\Agent\Identity;
  * @package App
  *
  * @property int $id
- * @property int $fitgapQuestionId
+ * @property int $fitgap_question_id
+ * @property int $vendor_application_id
+ * @property string $response
+ * @property string $comments
+ * @property int $score
  *
  */
 class FitgapVendorResponse extends Model
@@ -18,4 +22,29 @@ class FitgapVendorResponse extends Model
     public $guarded = [];
 
     protected $table = 'fitgap_vendor_responses';
+
+    public function question()
+    {
+        return $this->belongsTo(FitgapQuestion::class, 'fitgap_question_id');
+    }
+
+    public function vendorApplication()
+    {
+        return $this->belongsTo(VendorApplication::class, 'vendor_application_id');
+    }
+
+    public function response()
+    {
+        return $this->response;
+    }
+
+    public function comments()
+    {
+        return $this->comments;
+    }
+
+    public function score()
+    {
+        return $this->score;
+    }
 }
