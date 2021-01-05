e5cb38ad8dd2d444c2b6bb8c4cbf69754060fd41
Vicent Gisbert <vgisbert@microbio.tv>
2021-01-05 14:27:16 +0100

    use case view form open phase project fix

--- resources/views/components/accenture/projectNavbar.blade.php
+++ resources/views/components/accenture/projectNavbar.blade.php
@@ -46,8 +46,7 @@
                             <a class="pt-1px d-none d-md-block" href="{{route('accenture.projectView', ['project' => $project])}}">View/Edit project</a>
                         </li>
                         @endif
-{{--                            @if ($project->currentPhase != 'preparation' && $project->useCases !== 'no')--}}
-                        @if (false)
+                        @if ($project->currentPhase != 'preparation' && $project->useCases !== 'no')
                         <li class="header-link-item ml-3 pl-3 border-left d-flex align-items-center {{$section == 'useCasesSetUp' ? 'active' : ''}}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -56,7 +55,7 @@
                                 <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
                                 <polyline points="22 4 12 14.01 9 11.01"></polyline>
                             </svg>
-                            <a class="pt-1px d-none d-md-block" href="{{route('accenture.useCasesSetUp', ['project' => $project])}}">Use Cases Set Up</a>
+                            <a class="pt-1px d-none d-md-block" href="{{route('accenture.projectUseCasesSetUp', ['project' => $project])}}">Use Cases Set Up</a>
                         </li>
                         @endif
                         @if ($project->hasValueTargeting)
