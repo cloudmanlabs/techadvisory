4c414ff25f7410d3969884f47f2bafa31d188a0b
Vicent Gisbert <vgisbert@microbio.tv>
2021-02-04 14:50:14 +0100

    add "use case" management to backoffice

--- app/Nova/UseCaseQuestionResponse.php
+++ app/Nova/UseCaseQuestionResponse.php
@@ -19,98 +19,98 @@ use Laravel\Nova\Panel;
 
 class UseCaseQuestionResponse extends Resource
 {
-//    public static $displayInNavigation = false;
-//
-//    /**
-//     * The model the resource corresponds to.
-//     *
-//     * @var string
-//     */
-//    public static $model = 'App\UseCaseQuestionResponse';
-//
-//    /**
-//     * The single value that should be used to represent the resource when being displayed.
-//     *
-//     * @var string
-//     */
-//    public static $title = 'response';
-//
-//    /**
-//     * The columns that should be searched.
-//     *
-//     * @var array
-//     */
-//    public static $search = [
-//        'response',
-//    ];
-//
-//    /**
-//     * Get the fields displayed by the resource.
-//     *
-//     * @param  \Illuminate\Http\Request  $request
-//     * @return array
-//     */
-//    public function fields(Request $request)
-//    {
-//        $response = 'Question: ' . optional($this->originalQuestion)->label;
-//
-//        return [
-//            // ID::make()->sortable(),
-//            BelongsTo::make('Question', 'originalQuestion', 'App\Nova\UseCaseQuestion')
-//                ->hideWhenUpdating(),
-//            BelongsTo::make('Use Case', 'useCase', 'App\Nova\UseCase'),
-//
-//            Text::make('Practice', function(){
-//                return optional(optional($this->originalQuestion)->practice)->name;
-//            }),
-//
-//            Text::make('Response', 'response')
-//                ->help($response)
-//                ->hideWhenCreating(),
-//        ];
-//    }
-//
-//    /**
-//     * Get the cards available for the request.
-//     *
-//     * @param  \Illuminate\Http\Request  $request
-//     * @return array
-//     */
-//    public function cards(Request $request)
-//    {
-//        return [];
-//    }
-//
-//    /**
-//     * Get the filters available for the resource.
-//     *
-//     * @param  \Illuminate\Http\Request  $request
-//     * @return array
-//     */
-//    public function filters(Request $request)
-//    {
-//        return [];
-//    }
-//
-//    /**
-//     * Get the lenses available for the resource.
-//     *
-//     * @param  \Illuminate\Http\Request  $request
-//     * @return array
-//     */
-//    public function lenses(Request $request)
-//    {
-//        return [];
-//    }
-//
-//    /**
-//     * Get the actions available for the resource.
-//     *
-//     * @param  \Illuminate\Http\Request  $request
-//     * @return array
-//     */
-//    public function actions(Request $request)
-//    {
-//        return [];
-//    }
+    public static $displayInNavigation = false;
+
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\UseCaseQuestionResponse';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'response';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'response',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        $response = 'Question: ' . optional($this->originalQuestion)->label;
+
+        return [
+            // ID::make()->sortable(),
+            BelongsTo::make('Question', 'originalQuestion', 'App\Nova\UseCaseQuestion')
+                ->hideWhenUpdating(),
+            BelongsTo::make('Use Case', 'useCase', 'App\Nova\UseCase'),
+
+            Text::make('Practice', function(){
+                return optional(optional($this->originalQuestion)->practice)->name;
+            }),
+
+            Text::make('Response', 'response')
+                ->help($response)
+                ->hideWhenCreating(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
 }
